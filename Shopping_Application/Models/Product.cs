6eca3cf4c9037630f5cd7bde0fde6528b2243d52
Katha99 <57539645+Katha99@users.noreply.github.com>
2020-10-29 12:19:51 +0100

    products and carts added to the main site

--- Shopping_Application/Models/Product.cs
+++ Shopping_Application/Models/Product.cs
@@ -8,11 +8,8 @@ namespace Shopping_Application.Models
     public class Product
     {
         public string Id { get; set; }
-
         public string Name { get; set; }
-
         public double Price { get; set; }
-
         public string Photo { get; set; }
     }
 
