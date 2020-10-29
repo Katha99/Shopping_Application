68c423e5416e67b6c2a96ebfb5fb8454e5242103
Katha99 <57539645+Katha99@users.noreply.github.com>
2020-10-29 10:42:52 +0100

    Projektdateien hinzufügen.

---
+++ Shopping_Application/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping_Application.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Photo { get; set; }
+    }
+
+}
