5d5d0b0f89408faa2a353254330607bce567e106
Katha99 <57539645+Katha99@users.noreply.github.com>
2020-11-01 17:32:57 +0100

    random recomendations added

--- Shopping_Application/Controllers/HomeController.cs
+++ Shopping_Application/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Shopping_Application.Models;
 using DataLibrary;
+using static DataLibrary.Logic.ProductProcessor;
 
 namespace Shopping_Application.Controllers
 {
@@ -12,7 +13,40 @@ namespace Shopping_Application.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var data = LoadProduct();
+            List<Product> products = new List<Product>();
+
+            int number = 0, j = 0;
+            foreach (var row in data)
+            {
+                number++;
+                products.Add(new Product
+                {
+                    Id = row.Id,
+                    Titel = row.Titel,
+                    Price = row.Price,
+                    Photo = row.Photo,
+                    Content = row.Content,
+                    Author = row.Author
+                });
+            }
+
+            Random rnd = new Random();
+            List<int> randomList = new List<int>();
+            List<Product> randProducts = new List<Product>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                j = rnd.Next(0, number -1);
+                if (!randomList.Contains(j))
+                {
+                    randomList.Add(j);
+                    randProducts.Add(products[j]);
+                }
+            }
+            
+            ViewBag.products = randProducts;
+            return View(randProducts);
         }
 
         public ActionResult About()
