8d548e7ae9cfc3979856811e0ca2d24ac9bb6099
Katharina Rabanus <kathafunny@gmail.com>
2020-12-02 09:25:57 +0100

    The innitial migration and the database was updated

---
+++ MyBookstore/netzkern.MyBookstore.Data.EF/Migrations/202012011430407_InitialMigration.cs
@@ -0,0 +1,49 @@
+﻿namespace netzkern.MyBookstore.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Author",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(),
+                        Lastname = c.String(),
+                        Created = c.DateTime(nullable: false),
+                        Modified = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Photo = c.String(),
+                        Content = c.String(),
+                        Created = c.DateTime(nullable: false),
+                        Modified = c.DateTime(nullable: false),
+                        Author_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Author", t => t.Author_Id)
+                .Index(t => t.Author_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Product", "Author_Id", "dbo.Author");
+            DropIndex("dbo.Product", new[] { "Author_Id" });
+            DropTable("dbo.Product");
+            DropTable("dbo.Author");
+        }
+    }
+}
---
+++ MyBookstore/netzkern.MyBookstore.Data.EF/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace netzkern.MyBookstore.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<netzkern.MyBookstore.Data.EF.EfContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(netzkern.MyBookstore.Data.EF.EfContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
