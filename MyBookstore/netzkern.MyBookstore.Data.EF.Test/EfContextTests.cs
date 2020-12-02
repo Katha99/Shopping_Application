c8ae71d451c0fa81d001841e4dd0e4d68fb2b266
Katharina Rabanus <kathafunny@gmail.com>
2020-12-02 15:44:10 +0100

    Added testing for database creation

---
+++ MyBookstore/netzkern.MyBookstore.Data.EF.Test/EfContextTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using netzkern.MyBookstore.Data.EF;
+
+namespace netzkern.MyBookstore.Data.EF.Test
+{
+    [TestClass]
+    public class EfContextTests
+    {
+        [TestMethod]
+        public void EfContext_Can_create_database()
+        {
+            // Arrange
+            EfContext efContextTest = new EfContext();
+            if ( efContextTest.Database.Exists() )
+            {
+                efContextTest.Database.Delete();
+            }
+
+            // Act
+            efContextTest.Database.Create();
+
+            // Assert
+            Assert.IsTrue(efContextTest.Database.Exists());
+        }
+    }
+}
