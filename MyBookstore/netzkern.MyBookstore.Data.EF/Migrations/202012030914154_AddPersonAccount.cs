cccc0e5497737056b63b9ac0d9d7312e84a15085
Katharina Rabanus <kathafunny@gmail.com>
2020-12-03 10:54:15 +0100

    Update and Migration of the Database

---
+++ MyBookstore/netzkern.MyBookstore.Data.EF/Migrations/202012030914154_AddPersonAccount.cs
@@ -0,0 +1,37 @@
+﻿namespace netzkern.MyBookstore.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPersonAccount : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PersonAccount",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(),
+                        Lastname = c.String(),
+                        EmailAddress = c.String(),
+                        Password = c.String(),
+                        Created = c.DateTime(nullable: false),
+                        Modified = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Order", "PersonAccountId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Order", "PersonAccountId");
+            AddForeignKey("dbo.Order", "PersonAccountId", "dbo.PersonAccount", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Order", "PersonAccountId", "dbo.PersonAccount");
+            DropIndex("dbo.Order", new[] { "PersonAccountId" });
+            DropColumn("dbo.Order", "PersonAccountId");
+            DropTable("dbo.PersonAccount");
+        }
+    }
+}
