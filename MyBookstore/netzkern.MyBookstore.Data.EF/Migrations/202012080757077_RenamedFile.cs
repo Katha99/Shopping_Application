70e6c4fd5f094dceb36f5df5d1ac13678b65b294
Katharina Rabanus <kathafunny@gmail.com>
2020-12-08 09:05:29 +0100

    Update Database

---
+++ MyBookstore/netzkern.MyBookstore.Data.EF/Migrations/202012080757077_RenamedFile.cs
@@ -0,0 +1,16 @@
+﻿namespace netzkern.MyBookstore.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenamedFile : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
