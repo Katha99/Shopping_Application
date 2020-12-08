82afcf86ce4dbf6f9274ddd16e2201a6348f981b
Katharina Rabanus <kathafunny@gmail.com>
2020-12-08 08:59:54 +0100

    Added Tests for EfContext CRUD

--- MyBookstore/netzkern.MyBookstore.Data.EF.Test/EfContextTests.cs
+++ MyBookstore/netzkern.MyBookstore.Data.EF.Test/EfContextTests.cs
@@ -6,13 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 using netzkern.MyBookstore.Data.EF;
+using netzkern.MyBookstore.Model;
 
 namespace netzkern.MyBookstore.Data.EF.Test
 {
     [TestClass]
     public class EfContextTests
     {
-        [TestMethod]
+        //[TestMethod]
         public void EfContext_Can_create_database()
         {
             // Arrange
@@ -27,6 +28,58 @@ namespace netzkern.MyBookstore.Data.EF.Test
 
             // Assert
             Assert.IsTrue(efContextTest.Database.Exists());
+        }
+
+        [TestMethod]
+        public void EfContext_Crud_Author()
+        {
+            #region Author Test
+            // Arrange
+            Author author = new Author()
+            {
+                Firstname = $"Max_{Guid.NewGuid()}",
+                Lastname = "Mustermann"
+            };
+
+            string newFirstname = $"Moritz_{Guid.NewGuid()}";
+
+            // Act
+            using (EfContext efContext = new EfContext())
+            {
+                // Insert
+                efContext.Authors.Add(author);
+                efContext.SaveChanges();
+            }
+
+            using (EfContext efContext = new EfContext())
+            {
+                // Read ( Inserted )
+                Author loadedAuthors = efContext.Authors.Find(author.Id);
+                Assert.AreEqual(author.Firstname, loadedAuthors.Firstname);
+
+                // Update
+                loadedAuthors.Firstname = newFirstname;
+                efContext.SaveChanges();
+            }
+
+            using (EfContext efContext = new EfContext())
+            {
+                // Read Update
+                Author loadedAuthors = efContext.Authors.Find(author.Id);
+                Assert.AreEqual(loadedAuthors.Firstname, newFirstname);
+
+                // Delete
+                efContext.Authors.Remove(loadedAuthors);
+                efContext.SaveChanges();
+            }
+
+            using (EfContext efContext = new EfContext())
+            {
+                // Check Delete
+                Author loadedAuthor = efContext.Authors.Find(author.Id);
+                Assert.IsNull(loadedAuthor);
+            }
+            #endregion
         }
     }
 }
